937d056525b95ee8bd1578cab39ef2c8ee199513
Hao.Wu <1593774151@qq.com>
2023-03-07 18:00:20 +0800

    add callGPT.py

--- README.md
+++ README.md
@@ -7,4 +7,15 @@ eg:
 
 <img width="2297" alt="image" src="https://user-images.githubusercontent.com/54488712/222626605-614a5de6-16c6-44b3-ab78-7eb5a7282edb.png">
 
-api直连，目前国内给API墙了，需要后台服务器做转发，等下写下。
+api直连，目前国内给API墙了，我写了一个python脚本，callGPT.py
+```shell
+pip install flask
+pip install requests
+```
+
+把key放入，使用post请求
+http://localhost:8080/chatgpt
+{
+    "message": "要发送的消息"
+}
+。
---
+++ callGPT.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+import json
+import requests
+import time
+
+app = Flask(__name__)
+
+def print_message(message):
+    responseText = ""
+    index = 0
+
+    print(message)
+
+    print()
+
+@app.route('/chatgpt', methods=['POST'])
+def callCHATGPT():
+
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {key}"
+    }
+
+    data = request.json
+    message = data["message"]
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{ "role": "user", "content": message }]
+    }
+
+    url = "https://api.openai.com/v1/chat/completions"
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    if response.status_code == 200:
+        json_data = json.loads(response.text)
+        response = json_data["choices"][0]["message"]["content"]
+
+        print(response)
+        
+        print_message(response)
+
+        return response
+
+if __name__ == '__main__':
+    app.run(port=8080)
+
